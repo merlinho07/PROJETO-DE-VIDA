eccf248be6975610b2e9c228de3e0ee790d0c9bf
merlinho07 <160855159+merlinho07@users.noreply.github.com>
2025-05-06 11:43:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,22 @@ for (let i=0;  i <botoes.length; j++) {
 
 
 }
+ 
+const contadores = document .querySelectorAll(".contador");
+const tempoObjetivol = new Date("2025-10-05T00:00:00");
+
+contadores[0].textContent = calculatempo(tempoObjetivol);
+
+function calculatempo(tempoObjetivol){
+    let tempoatual = new Date ();
+    let tempofinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %=24;
+
+    return dias+ " dias "  +  horas  +  " horas " + minutos + " minutos " + segundos + " segundos "
+}
